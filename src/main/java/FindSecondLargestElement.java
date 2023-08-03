428b6ddab57bd947f2c6fb5bf86f06e2a9919c13
jainkomal153 <jainkomal153@gmail.com>
2023-08-03 10:43:16 +0530

    finding the second greatest element in an array

--- src/main/java/FindSecondLargestElement.java
+++ src/main/java/FindSecondLargestElement.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class FindSecondLargestElement {
+import java.util.*;
+
+public class FindSecondLargestElement {
+    public int secondLargestElement(int arr[], int n){
+        if(n<=1){
+            return -1;
+        }
+       Arrays.sort(arr);
+        for(int i=n-2; i>=0; i--){
+            if(arr[i]<arr[i+1]){
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr= {10,10,6,7};
+        FindSecondLargestElement findSecondLargestElement = new FindSecondLargestElement();
+        System.out.println(findSecondLargestElement.secondLargestElement(arr, arr.length));
+    }
+
 }
