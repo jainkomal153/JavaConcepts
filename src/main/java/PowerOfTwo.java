db74015c865894ff50aaac6c2e68de5c21fb9a27
jainkomal153 <jainkomal153@gmail.com>
2023-08-18 14:00:57 +0530

    updating the power of two program with better time complexity O(1)

--- src/main/java/PowerOfTwo.java
+++ src/main/java/PowerOfTwo.java
@@ -16,8 +16,18 @@ public class PowerOfTwo {
         return num;
     }
 
+
+    public boolean isPowerOfTwoUsingBinaryOperator(int num){
+        int result = num !=0 ? num & num-1: 1;
+        if(result ==0){
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         PowerOfTwo powerOfTwo = new PowerOfTwo();
-        System.out.println(powerOfTwo.isPowerOfTwo(16));
+        System.out.println(powerOfTwo.isPowerOfTwo(0));
+        System.out.println(powerOfTwo.isPowerOfTwoUsingBinaryOperator(0));
     }
 }
