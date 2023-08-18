41a3156684c623e98aa445d3fd0e29b670133edd
jainkomal153 <jainkomal153@gmail.com>
2023-08-18 14:00:11 +0530

    checking if the string is palindrome i.e. reverse of characters should be the same as string like 'abba'

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Palindrome {
+public class Palindrome {
+
+    public static void main(String[] args) {
+        String s= "abb";
+        Palindrome palindrome = new Palindrome();
+        System.out.println(palindrome.isPalindrome(s));
+    }
+
+    private boolean isPalindrome(String s) {
+        StringBuilder string = new StringBuilder(s);
+        if(s.equalsIgnoreCase(String.valueOf(string.reverse()))){
+            return true;
+        }
+        return false;
+    }
 }
